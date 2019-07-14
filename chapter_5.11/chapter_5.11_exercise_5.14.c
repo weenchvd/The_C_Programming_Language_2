1aed629a8deade64350436a2d68ba37350f1737f
weenchvd <d88744@gmail.com>
2019-07-14 18:31:48 +0300

    Chapter 5.11 | Exercise 5.16

--- chapter_5.11/chapter_5.11_exercise_5.14.c
+++ chapter_5.11/chapter_5.11_exercise_5.14.c
@@ -1,4 +1,4 @@
-/** Chapter 5.11 | Exercise 5.15 */
+/** Chapter 5.11 | Exercise 5.16 */
 
 #include <stdio.h>
 #include <string.h>
@@ -10,13 +10,13 @@
 
 char* lineptr[MAXLINES];		/* pointers to text lines */
 char lines[MAXLENGTH];			/* array for lines */
-char lowercaselines[MAXLENGTH];
+char lc[MAXLENGTH];				/* array for lower case lines */
 
 int ngetline(char* ps, int lim, char n);
 int readlines(char* lineptr[], char* plines, int maxlines);
 void writelines(char* lineptr[], int nlines);
 
-void qsort(void* lineptr[], int left, int right, int (*comp) (void*, void*), int fold);
+void qsort(void* lineptr[], int left, int right, int (*comp) (void*, void*), int fold, int dir);
 int numcmp(char* s1, char* s2);
 
 /* sort input lines */
@@ -24,26 +24,41 @@ int main(int argc, char* argv[])
 {
 	int nlines;					/* number of input lines read */
 	int numeric = 0;			/* 1 if numeric sort */
-	int decr = 0;
+	int rev = 0;
 	int fold = 0;
+	int dir = 0;
 	int i, j;
-	for (i = 1; argc-- > 1; i++) {
-		if (strcmp(argv[i], "-n") == 0) {
-			numeric = 1;
-		}
-		else if (strcmp(argv[i], "-r") == 0) {
-			decr = 1;
-		}
-		else if (strcmp(argv[i], "-f") == 0) {
-			fold = 1;
+	char c;
+	if (argc > 1 && **++argv == '-') {
+		for (i = 1; (c = *(*argv + i)) != '\0'; i++) {
+			switch (c) {
+			case ('n'):
+				numeric = 1;
+				break;
+			case ('r'):
+				rev = 1;
+				break;
+			case ('f'):
+				fold = 1;
+				break;
+			case ('d'):
+				dir = 1;
+				break;
+			default:
+				printf("---Error: unknown argument\n");
+				break;
+			}
 		}
 	}
 	if (numeric && fold) {
 		fold = 0;
 	}
+	if (numeric && dir) {
+		dir = 0;
+	}
 	if ((nlines = readlines(lineptr, lines, MAXLINES)) >= 0) {
-		qsort((void**)lineptr, 0, nlines - 1, (int (*) (void*, void*)) (numeric ? numcmp : strcmp), fold);
-		if (decr) {
+		qsort((void**)lineptr, 0, nlines - 1, (int (*) (void*, void*)) (numeric ? numcmp : strcmp), fold, dir);
+		if (rev) {
 			void swap(void* v[], int i, int j);
 			for (i = 0, j = nlines - 1; i < j; i++, j--) {
 				swap((void**)lineptr, i, j);
@@ -86,44 +101,52 @@ void writelines(char* lineptr[], int nlines)
 }
 
 /* qsort: sort v[left]...v[right] into increasing order */
-void qsort(void* v[], int left, int right, int (*comp) (void*, void*), int fold)
+void qsort(void* v[], int left, int right, int (*comp) (void*, void*), int fold, int dir)
 {
 	int i, last;
+	void* viptr, * vleftptr;
+	int j, len;
 	void swap(void* v[], int i, int j);
 	if (left >= right) {			/* do nothing if array contains */
 		return;						/* fewer than two elements */
 	}
 	swap(v, left, (left + right) / 2);
 	last = left;
-	if (fold) {
-		void* viptr, * vleftptr;
-		int j, len;
-		len = 0;
-		for (i = left + 1; i <= right; i++) {
-			viptr = (void*)strcpy(lowercaselines, (const char*)v[i]);
-			len = strlen(lowercaselines);
-			for (j = 0; lowercaselines[j] != '\0'; j++) {
-				lowercaselines[j] = (char)tolower((int)lowercaselines[j]);
+	for (i = left + 1; i <= right; i++) {
+		if (fold || dir) {
+			len = 0;
+			viptr = (void*)strcpy(lc, (const char*)v[i]);
+			len = strlen(lc);
+			for (j = 0; lc[j] != '\0'; j++) {
+				if (fold) {
+					lc[j] = (char)tolower((int)lc[j]);
+				}
+				if (dir && !(isalnum(lc[j]) || isspace(lc[j]))) {
+					lc[j] = ' ';
+				}
 			}
-			vleftptr = (void*)strcpy(lowercaselines + len + 1, (const char*)v[left]);
-			for (j = len + 1; lowercaselines[j] != '\0'; j++) {
-				lowercaselines[j] = (char)tolower((int)lowercaselines[j]);
+			vleftptr = (void*)strcpy(lc + len + 1, (const char*)v[left]);
+			for (j = len + 1; lc[j] != '\0'; j++) {
+				if (fold) {
+					lc[j] = (char)tolower((int)lc[j]);
+				}
+				if (dir && !(isalnum(lc[j]) || isspace(lc[j]))) {
+					lc[j] = ' ';
+				}
 			}
 			if ((*comp) (viptr, vleftptr) < 0) {
 				swap(v, ++last, i);
 			}
 		}
-	}
-	else {
-		for (i = left + 1; i <= right; i++) {
+		else {
 			if ((*comp) (v[i], v[left]) < 0) {
 				swap(v, ++last, i);
 			}
 		}
 	}
 	swap(v, left, last);
-	qsort(v, left, last - 1, comp, fold);
-	qsort(v, last + 1, right, comp, fold);
+	qsort(v, left, last - 1, comp, fold, dir);
+	qsort(v, last + 1, right, comp, fold, dir);
 }
 
 void swap(void* v[], int i, int j)
