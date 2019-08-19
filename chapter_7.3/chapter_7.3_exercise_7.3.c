825e9a1a025f7073dc8a18e7a91177d110319aa2
weenchvd <d88744@gmail.com>
2019-08-19 21:01:31 +0300

    Chapter 7.3 | Exercise 7.3

---
+++ chapter_7.3/chapter_7.3_exercise_7.3.c
@@ -0,0 +1,107 @@
+/** Chapter 7.3 | Exercise 7.3 */
+
+#include <stdio.h>
+#include <stdarg.h>
+
+int main()
+{
+	void minprintf(char* fmt, ...);
+
+	char* string = "It's a just string";
+	char c = 'A';
+	int i = -100;
+	double d = 211643747.555;
+	double de = 0.000000351;
+	unsigned int ui = 3999456;
+	void* ps = (void*) string;
+	minprintf("String: %s\t\tSingle character: %c\n", string, c);
+	minprintf("Unsigned decimal: %u\t\tSigned decimal: %d\n", ui, i);
+	minprintf("Unsigned octal: %o\t\tUnsigned hexadecimal: %x\n", ui, ui);
+	minprintf("Decimal floating-point: %f or %e, %f of %e\n", d, d, de, de);
+	minprintf("Percent character: %%\t\tBackslash character \b: \\\\\n");
+	minprintf("JIBBERJABBER_JIBBERJABBER_JIBBERJABBER\rPointer: %p", ps);
+	return 0;
+}
+
+/* minprintf: minimal printf with variable argument list */
+void minprintf(char* fmt, ...)
+{
+	va_list ap;				/* points to each unnamed arg in turn */
+	char* p, * sval;
+	char c;
+	int ival;
+	unsigned int uival;
+	double dval;
+	void* ptr;
+	va_start(ap, fmt);		/* make ap point to 1st unnamed arg */
+	for (p = fmt; *p; p++) {
+		if (*p == '%') {
+			switch (*++p) {
+			case 'd':
+				ival = va_arg(ap, int);
+				printf("%d", ival);
+				break;
+			case 'u':
+				uival = va_arg(ap, unsigned int);
+				printf("%u", uival);
+				break;
+			case 'o':
+				uival = va_arg(ap, unsigned int);
+				printf("%o", uival);
+				break;
+			case 'x':
+				uival = va_arg(ap, unsigned int);
+				printf("%x", uival);
+				break;
+			case 'f':
+				dval = va_arg(ap, double);
+				printf("%f", dval);
+				break;
+			case 'e':
+				dval = va_arg(ap, double);
+				printf("%e", dval);
+				break;
+			case 's':
+				for (sval = va_arg(ap, char*); *sval; sval++) {
+					putchar(*sval);
+				}
+				break;
+			case 'c':
+				c = va_arg(ap, char);
+				putchar(c);
+				break;
+			case 'p':
+				ptr = va_arg(ap, void*);
+				printf("%p", ptr);
+				break;
+			default:
+				putchar(*p);
+				break;
+			}
+		}
+		else if (*p == '\\') {
+			switch (*++p) {
+			case 'n':
+				putchar('\n');
+				break;
+			case 't':
+				putchar('\t');
+				break;
+			case 'b':
+				putchar('\b');
+				break;
+			case 'r':
+				putchar('\r');
+				break;
+			case '\\':
+				putchar('\\');
+				break;
+			default:
+				putchar(*p);
+				break;
+			}
+		}
+		else putchar(*p);
+	}
+	va_end(ap);				/* clean up when done */
+}
